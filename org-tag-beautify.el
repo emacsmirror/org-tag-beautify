040b64b9370edf728eba32dda1d39229a5debf6c
stardiviner <numbchild@gmail.com>
2021-01-09 13:34:31 +0800

    Create source code file

---
+++ org-tag-beautify.el
@@ -0,0 +1,44 @@
+;;; org-tag-beautify.el --- Beautify Org Mode tags. -*- lexical-binding: t; -*-
+;; -*- coding: utf-8 -*-
+
+;;; Time-stamp: <2021-01-09 13:34:22 stardiviner>
+
+;; Copyright (C) 2020-2021 Free Software Foundation, Inc.
+;; The source code is licensed under GPLv3.
+;; The image data is NOT licensed under GPLv3.
+
+;; Authors: stardiviner <numbchild@gmail.com>
+;; Package-Requires: ((emacs "26.1"))
+;; Package-Version: 0.1.0
+;; Keywords: org
+;; homepage: https://github.com/stardiviner/org-tag-beautify
+
+;; org-tag-beautify is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; org-tag-beautify is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
+;;
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Usage:
+;;
+;; (org-tag-beautify-mode 1)
+
+;;; Code:
+
+
+
+
+
+(provide 'org-tag-beautify)
+
+;;; org-tag-beautify.el ends here
