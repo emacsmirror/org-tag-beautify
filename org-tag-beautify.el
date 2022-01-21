c1b64893deeaab11a9a34e3d07bef37eabc7b707
stardiviner <numbchild@gmail.com>
2022-01-21 15:54:18 +0800

    add "keyboard" tag icon

--- org-tag-beautify.el
+++ org-tag-beautify.el
@@ -338,6 +338,7 @@
                   ("mobile" . ,(all-the-icons-faicon "mobile" :face '(:foreground "SkyBlue")))
                   ("smartphone" . ,(all-the-icons-material "smartphone" :face '(:foreground "SkyBlue")))
                   ("phone" . ,(all-the-icons-faicon "phone" :face '(:foreground "SkyBlue")))
+                  ("keyboard" . ,(all-the-icons-faicon "keyboard-o" :face '(:foreground "cyan")))
                   ("microphone" . ,(all-the-icons-faicon "microphone" :face '(:foreground "SkyBlue")))
                   ("router" . ,(all-the-icons-material "router" :face '(:foreground "SkyBlue")))
                   ("fax" . ,(all-the-icons-faicon "fax" :face '(:foreground "DarkSlateBlue")))
